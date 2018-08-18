6247a7816d4f175b8c76fee64c84079632d9c548
July541 <zhulei1995@126.com>
2018-08-18 21:24:16 +0800

    add mysql config and database model

--- fouryears/models.py
+++ fouryears/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Letters(models.Model):
+    from_name = models.CharField(max_length=200)
+    to_name = models.CharField(max_length=200)
+    from_url = models.CharField(max_length=200)
+    to_url = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    send_date = models.DateTimeField()
+    ip = models.CharField(max_length=200)
+    is_del = models.BooleanField()
+
+    
